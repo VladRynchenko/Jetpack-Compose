e882fde327251b8fe27b31c532cf126e3ce4eb0d
VladRynchenko <41292054+VladRynchenko@users.noreply.github.com>
2021-07-21 18:01:19 +0300

    Using LazyColumn

--- app/src/main/java/com/example/composes/MainActivity.kt
+++ app/src/main/java/com/example/composes/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -35,7 +37,10 @@ fun Greeting(name: String) {
 
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(onClick = { updateCount(count + 1) }) {
+    Button(
+        onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(backgroundColor = if (count > 5) Color.Cyan else Color.White)
+    ) {
         Text("I've been clicked $count times")
     }
 }
@@ -58,19 +63,23 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
     val counterState = remember { mutableStateOf(0) }
     Column(modifier = Modifier.fillMaxHeight()) {
-        Column(modifier = Modifier.weight(1f)) {
-            for (name in names) {
-                Greeting(name = name)
-                Divider(color = Color.Black)
-            }
-        }
-        Divider(color = Color.Transparent, thickness = 32.dp)
+        NameList(names = names, modifier = Modifier.weight(1f))
         Counter(count = counterState.value, updateCount = { newValue ->
             counterState.value = newValue
         })
+    }
 
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
     }
 }
